d41a9ab567b5406b223009373b0fa421300ef9fe
leejm <leejm@mz.co.kr>
2021-03-13 07:37:21 +0900

    promisify 수정. _.findAsync, _.promisify, _.then, _.otherwise, _.finally unit test 추가

--- test/app.spec.js
+++ test/app.spec.js
@@ -8,18 +8,43 @@ const expect = chai.expect;
 
 const arr = [1, 2, 3, 4, 5];
 
+describe('# _.not test', () => {
+  it('_.not(true) should return false', () => {
+    expect(_.not(true)).to.eqls(false);
+  });
+
+  it('_.not("str") should return false', () => {
+    expect(_.not('str')).to.eqls(false);
+  });
+
+  it('_.not(false) should return true', () => {
+    expect(_.not(false)).to.eqls(true);
+  });
+
+  it('_.not(undefined) should return true', () => {
+    expect(_.not(undefined)).to.eqls(true);
+  });
+
+  it('_.not(null) should return true', () => {
+    expect(_.not(null)).to.eqls(true);
+  });
+
+  it('_.not(0) should return true', () => {
+    expect(_.not(0)).to.eqls(true);
+  });
+});
+
 describe('# _.mapAsync test', () => {
   it('should return all values multiplied by 2', async () => {
     const asyncMapper = (a) =>
       new Promise((resolve) => {
         setTimeout(() => {
           resolve(2 * a);
-        }, 300);
+        }, 5);
       });
 
     const results = await _.mapAsync(asyncMapper, arr);
-    console.log(results);
-    expect(results).to.eql([2, 4, 6, 8, 10]);
+    expect(results).to.eqls([2, 4, 6, 8, 10]);
   });
 });
 
@@ -29,22 +54,21 @@ describe('# _.filterAsync test', () => {
       new Promise((resolve) => {
         setTimeout(() => {
           resolve(!(0 === a % 2));
-        }, 300);
+        }, 5);
       });
     const results = await _.filterAsync(asyncFilter, arr);
-    console.log(results);
     expect(results).not.empty;
-    expect(results).to.eql([1, 3, 5]);
+    expect(results).to.eqls([1, 3, 5]);
   });
 });
 
 describe('# _.reduceAsync test', () => {
-  it('should return all values mulplied by 4', async () => {
+  it('should return all values mulplied by 2', async () => {
     const asyncMapper = (a) =>
       new Promise((resolve) => {
         setTimeout(() => {
           resolve(2 * a);
-        }, 500);
+        }, 5);
       });
 
     const results = await _.reduceAsync(
@@ -59,34 +83,110 @@ describe('# _.reduceAsync test', () => {
       arr
     );
 
-    console.log(results);
     expect(results).not.empty;
-    expect(results).to.eql([2, 4, 6, 8, 10]);
+    expect(results).to.eqls([2, 4, 6, 8, 10]);
   });
 });
 
-describe('# _.not test', () => {
-  it('_.not(true) should return false', () => {
-    expect(_.not(true)).to.eql(false);
+describe('# _.findAsync test', () => {
+  it('should return object name property "hello"', async () => {
+    const arr = [
+      { name: 'hi', age: 21 },
+      { name: 'hello', age: 22 },
+      { name: 'alo', age: 23 }
+    ];
+    const asyncFilter = (a) =>
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(_.pipe(_.get('name'), _.equals('hello'))(a));
+        }, 5);
+      });
+
+    const result = await _.findAsync(asyncFilter, arr);
+
+    expect(result).to.have.own.property('name', 'hello');
+    expect(result).to.eqls({ name: 'hello', age: 22 });
+  });
+});
+
+describe('# _.promisify test', () => {
+  it('should return array [128, 128, 128]', async () => {
+    const result = await _.promisify(128);
+    const result1 = await _.promisify((a, b) => a + b, 64, 64);
+    const result2 = await _.promisify(Promise.resolve(128));
+
+    expect([result, result1, result2]).to.eqls([128, 128, 128]);
+  });
+});
+
+describe('# _.then test', () => {
+  it('should return 128', async () => {
+    const p = (a) =>
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(2 * a);
+        }, 5);
+      });
+
+    const composer = _.pipe(p, _.then(_.identity));
+    const result1 = await composer(64);
+    const result2 = await _.then(_.identity, p(64));
+    const result3 = await _.pipe(
+      p,
+      _.then((x) => x / 2)
+    )(128);
+
+    expect([result1, result2, result3]).to.eqls([128, 128, 128]);
+  });
+});
+
+describe('# _.otherwise test', () => {
+  const p = (a) =>
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (_.equals(a * a, a)) {
+          resolve(a);
+        } else {
+          reject(new Error('wrong'));
+        }
+      });
+    });
+  const composer = _.pipe(p, _.then(_.identity), _.otherwise(_.identity));
+  it('should return "wrong" text', async () => {
+    const result = await composer(2);
+
+    expect(result).to.be.a('error', 'wrong');
   });
 
-  it('_.not("str") should return false', () => {
-    expect(_.not('str')).to.eql(false);
-  });
+  it('should return 1', async () => {
+    const result = await composer(1);
 
-  it('_.not(false) should return true', () => {
-    expect(_.not(false)).to.eql(true);
+    expect(result).to.be.a('number', 1);
   });
+});
 
-  it('_.not(undefined) should return true', () => {
-    expect(_.not(undefined)).to.eql(true);
-  });
+describe('# _.finally test', () => {
+  let isLoading = true;
+  const p = (a) =>
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (_.equals(a * a, a)) {
+          resolve(a);
+        } else {
+          reject(new Error('wrong'));
+        }
+      });
+    });
+  const composer = _.pipe(
+    p,
+    _.then(_.identity),
+    _.otherwise(_.identity),
+    _.finally(() => (isLoading = false))
+  );
 
-  it('_.not(null) should return true', () => {
-    expect(_.not(null)).to.eql(true);
-  });
+  it('loading should be false', async () => {
+    await composer(1);
 
-  it('_.not(0) should return true', () => {
-    expect(_.not(0)).to.eql(true);
+    expect(isLoading).to.be.a('boolean', false);
   });
 });
